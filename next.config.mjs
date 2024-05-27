1569b30d6d31b97eebdf7dff4fc1b7d605bcbf31
Normo26 <armand@tesla-mag.com>
2024-05-27 21:08:20 +0200

    updated config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+          {
+            source: '/api/:path*',
+            destination: 'https://www.tesla-mag.com/wp-json/wp/v2/:path*', // Proxy to the external API
+          },
+        ]
+      },
+};
 
 export default nextConfig;
